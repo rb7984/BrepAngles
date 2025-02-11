0e295b50da800f2617049663f13bc6d9a20ed0eb
riccbarelli <barelliriccardo@gmail.com>
2025-02-11 12:46:32 +0100

    functioning prototype
    input a Brepp, outputs a list of strings with all angles

--- DihedralAngle/DihedralAngleComponent.cs
+++ DihedralAngle/DihedralAngleComponent.cs
@@ -1,7 +1,9 @@
 ﻿using Grasshopper.Kernel;
 using Rhino.Geometry;
+using Rhino.UI;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 // In order to load the result of this wizard, you will also need to
 // add the output bin/ folder of this project to the list of loaded
@@ -22,7 +24,7 @@ namespace DihedralAngle
         public DihedralAngleComponent()
           : base("DihedralAngle", "Nickname",
               "Description",
-              "General", "Utils")
+              "Surface", "Util")
         {
         }
 
@@ -31,6 +33,7 @@ namespace DihedralAngle
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddBrepParameter("Brep", "B", "A planar Brep to be evaluated", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -38,6 +41,7 @@ namespace DihedralAngle
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddGenericParameter("Output", "O", "List of Angles", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -47,6 +51,50 @@ namespace DihedralAngle
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            List<string> strings = new List<string>();
+
+            Brep buildingBody = new Brep();
+            DA.GetData(0, ref buildingBody);
+
+            foreach (BrepFace face in buildingBody.Faces)
+            {
+                int[] edgesIndex = face.AdjacentEdges();
+
+                List<BrepEdge> surroundingEdges = buildingBody.Edges.Where(e => edgesIndex.Contains(e.EdgeIndex)).ToList();
+
+                Vector3d faceNormal = face.NormalAt(0.5, 0.5);
+
+                Rhino.RhinoDoc.ActiveDoc.Objects.Add(new TextDot(face.FaceIndex.ToString(), face.GetBoundingBox(false).Center));
+
+                foreach (BrepEdge edge in surroundingEdges)
+                {
+                    Curve edgeCurve = edge.DuplicateCurve();
+                    edgeCurve.Domain = new Interval(0, 1);
+
+                    int[] neighbourFacesByFace = face.AdjacentFaces();
+                    int[] neighbourFacesByEdge = edge.AdjacentFaces();
+
+                    int[] neighbourFaces = neighbourFacesByEdge.Intersect(neighbourFacesByFace).ToArray();
+
+                    List<BrepFace> neighbourFace = buildingBody.Faces.Where(f => neighbourFaces.Contains(f.FaceIndex)).ToList();
+
+                    Curve edgeAsCurve = edge.ToNurbsCurve();
+                    double parameter;
+                    edgeAsCurve.ClosestPoint(face.GetBoundingBox(true).Center, out parameter);
+
+                    Vector3d scalingVector = edgeAsCurve.PointAt(parameter) - face.GetBoundingBox(true).Center;
+
+                    scalingVector.Unitize();
+
+                    double dihedralAngle = PreCalculate(edge, faceNormal, face, neighbourFace[0]);
+
+                    Rhino.RhinoDoc.ActiveDoc.Objects.Add(new TextDot(edge.EdgeIndex.ToString(), edge.GetBoundingBox(false).Center));
+
+                    strings.Add(face.FaceIndex.ToString() + "-" + edge.EdgeIndex.ToString() + ": " + dihedralAngle.ToString());
+                }
+            }
+
+            DA.SetDataList(0, strings);
         }
 
         /// <summary>
@@ -72,5 +120,69 @@ namespace DihedralAngle
         {
             get { return new Guid("f5339064-5447-4e67-9be4-7d663dc3a224"); }
         }
+
+        #region Auxiliary Methods
+        public double PreCalculate(BrepEdge edge, Vector3d faceNormal, BrepFace face, BrepFace adjacentFace)
+        {
+            Vector3d adjacentFaceNormal = adjacentFace.NormalAt(0.5, 0.5);
+
+            var loop = face.OuterLoop;
+            Curve loopasacurve = loop.To3dCurve();
+            Polyline loopasapolyline = new Polyline();
+
+            if (loopasacurve.IsPolyline())
+            {
+                //TODO maximum length può causare dei danni?
+                loopasapolyline = loopasacurve.ToPolyline(0.1, 0.1, 0.1, 3000).ToPolyline();
+            }
+
+            loopasapolyline.MergeColinearSegments(0.1, true);
+            Line[] ll = loopasapolyline.GetSegments();
+            Line line = new Line();
+
+            double distance = 100;
+            int counter = 0;
+            while (distance > 1)
+            {
+                double t;
+                if (edge.ClosestPoint(ll[counter].PointAt(0.5), out t))
+                {
+                    distance = edge.PointAt(t).DistanceTo(ll[counter].PointAt(0.5));
+                    line = ll[counter];
+                    counter++;
+                }
+            }
+
+            Vector3d testEdgeVector = line.Direction;
+            testEdgeVector.Unitize();
+
+            Vector3d rotatedFaceNormal = new Vector3d(faceNormal);
+            rotatedFaceNormal.Rotate(Math.PI / 2, testEdgeVector);
+
+            double rotatedDotProduct = Vector3d.Multiply(rotatedFaceNormal, adjacentFaceNormal);
+
+            return CalculateDihedralAngle(faceNormal, adjacentFaceNormal, rotatedDotProduct);
+        }
+
+        public double CalculateDihedralAngle(Vector3d normal1, Vector3d normal2, double dotProductRotated)
+        {
+            normal1.Unitize();
+            normal2.Unitize();
+
+            double angle = Vector3d.VectorAngle(normal1, normal2);
+
+            if (dotProductRotated > 0)
+            {
+                angle = Math.PI - angle;
+            }
+            else
+            {
+                angle = Math.PI + angle;
+            }
+
+            return angle;
+        }
+
+        #endregion
     }
 }
